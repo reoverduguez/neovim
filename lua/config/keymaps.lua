64a36b3275cb64103644e884efc3a89d2ad18c91
reoverduguez <reoverduguez@gmail.com>
2026-08-28 11:10:44 -0400

    feat: Installed neotree and telescope plugins. Added keymaps for navigating/managing buffers and windows.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,11 +5,18 @@ local keymap = vim.keymap.set
 
 keymap("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc ="Clears search result highlighting" })
 
--- Window navigation
-keymap("n", "<C-h>", "<C-w>h")
-keymap("n", "<C-j>", "<C-w>j")
-keymap("n", "<C-k>", "<C-w>k")
-keymap("n", "<C-l>", "<C-w>l")
+keymap("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Move to down window" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Move to up window" })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+keymap("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
+keymap("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+
+keymap("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+keymap("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
 
 keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
@@ -20,3 +27,6 @@ keymap("n", "<C-u>", "<C-u>zz", { desc = "Keep cursor centered when moving up" }
 keymap("v", ">", ">gv", { desc = "Indent selection and keep focus" })
 keymap("v", "<", "<gv", { desc = "De-indent selection and keep focus" })
 
+keymap("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+keymap("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
+keymap("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
